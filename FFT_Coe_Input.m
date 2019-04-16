1570d4dad3f1f0d1ccf11b05117fa00c591f7d3a
Ilayda Yaman <37779359+ilaydayaman@users.noreply.github.com>
2019-04-16 10:36:51 +0200

    Update FFT_Coe_Input.m

--- FFT_Coe_Input.m
+++ FFT_Coe_Input.m
@@ -1,17 +1,17 @@
 N = 2048;
 
-% % FFT Coefficients
-% % Generation of coefficients
-% FID = fopen('fft_coefficients.txt','w+');
-% 
-% for kn=0 : 1 : N/2 -1
-%         w = exp(-i*2*pi*kn/N);
-%         re=real(w);
-%         im=imag(w);
-%        fprintf(FID,'%f\n',re);
-%        fprintf(FID,'%f\n',im);
-% end
-% fclose(FID);
+% FFT Coefficients
+% Generation of coefficients
+FID = fopen('fft_coefficients.txt','w+');
+
+for kn=0 : 1 : N/2 -1
+        w = exp(-i*2*pi*kn/N);
+        re=real(w);
+        im=imag(w);
+       fprintf(FID,'%f\n',re);
+       fprintf(FID,'%f\n',im);
+end
+fclose(FID);
 
 
 % FFT Inputs
